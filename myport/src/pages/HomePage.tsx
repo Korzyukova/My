e2e9ad8467481b362ed65763e357e16924afdfa1
Tatiana Korzyukova <korzioukova@gmail.com>
2025-08-24 22:52:18 -0400

    pic and text

--- myport/src/pages/HomePage.tsx
+++ myport/src/pages/HomePage.tsx
@@ -70,16 +70,16 @@ export default function HomePage({
         transition={{ duration: 0.6 }}
   
       >
-        <div className="flex flex-col md:flex-row items-center md:items-stretch gap-8">
+        <div className="flex flex-col md:flex-row items-center md:items-start gap-8">
           {/* Блок с фото */}
-          <div className="flex-shrink-0 w-full md:w-1/2">
+          <div className="flex-shrink-0 w-full md:w-1/2 rounded-lg shadow-md overflow-hidden"> {/* Изменено: md:w-2/3 */}
             <img src="/me2.jpg" alt="Tatiana Korzyukova" className="w-full h-full object-cover" />
           </div>
           {/* Блок с описанием */}
-          <div className="flex-1 text-center md:text-left">
+          <div className="flex-1 text-justify md:w-1/2"> {/* Изменено: md:w-1/3 */}
             <p className="text-lg leading-relaxed">
               Hello, my name is Tatiana. I was born in Chukotka, Russia, which is near the Arctic Circle and Alaska. 
-               I later moved to Moscow for my Master's degree 
+              I later moved to Moscow for my Master's degree 
               in Aerospace Engineering. Due to the tense situation in my home country, I moved to the United States
               where I mastered a new language and a new profession as a Front-end Developer. Currently,
               I am going through a Data Science bootcamp. That's my story in a nutshell. You can find more information 
@@ -99,7 +99,7 @@ export default function HomePage({
         transition={{ duration: 0.6 }}
       
       >
-        <div className="flex flex-wrap gap-6">
+        <div className="flex flex-wrap justify-center gap-6">
           <a href="https://www.linkedin.com/in/tatiana-korzyukova/" className="flex items-center space-x-2 hover:text-blue-600"><Linkedin /><span>LinkedIn</span></a>
           <a href="https://www.instagram.com/korzyukova/#" className="flex items-center space-x-2 hover:text-pink-600"><Instagram /><span>Instagram</span></a>
           <a href="https://github.com/Korzyukova" className="flex items-center space-x-2 hover:text-gray-600"><Github /><span>GitHub</span></a>
